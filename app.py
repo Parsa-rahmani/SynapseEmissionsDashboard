a1aec601b8823c3c6218619a13f73d742714404f
Parsa Rahmani <parzarahmani@gmail.com>
2024-08-11 20:14:30 -0700

    fixed?????

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
 from minicheftracker import get_synapse_values
 from flask import render_template
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,19 +19,18 @@ def get_emissions():
     total_emissions = 0
     error_occurred = False
     for value in values:
-        if value['value'] == "RPC error":
-            result_text += f"Monthly $SYN emissions on {value['network_name']}: RPC error<br>"
+        if value['value'] == 'RPC error':
             error_occurred = True
-        else:
-            result_text += f"Monthly $SYN emissions on {value['network_name']}: {value['value']}<br>"
+        result_text += f"Monthly $SYN emissions on {value['network_name']}: {value['value']}<br>"
+        if value['value'] != 'RPC error':
             total_emissions += float(value['value'].replace(',', ''))
-
+    
     if not error_occurred:
         result_text += f"Total Monthly Emissions: {total_emissions:,.0f}"
     else:
-        result_text += "Total Monthly Emissions: Could not calculate due to RPC error(s)"
-
+        result_text += "Error occurred in retrieving some data."
     return result_text
+
 
 
 if __name__ == '__main__':
