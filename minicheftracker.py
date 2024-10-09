302d936faf1f6e229ccfdd4aa3889defeac7fc1b
parsa <parzarahmani@gmail.com>
2024-10-09 15:40:47 -0700

    kk

--- minicheftracker.py
+++ minicheftracker.py
@@ -46,15 +46,10 @@ chains_info = [
         'rpc_url': 'https://metis.drpc.org'
     },
     {
-        'network_name': 'Boba',
-        'contract_address': '0xd5609cD0e1675331E4Fb1d43207C8d9D83AAb17C',
-        'rpc_url': 'https://boba-eth.drpc.org'
+        'network_name': 'Blast',
+        'contract_address': '0x3100dC8464A8523306c3C5034de24a8927d6E590',
+        'rpc_url': 'https://blast.drpc.org'
     }
-    # {
-    #     'network_name': 'Blast',
-    #     'contract_address': '0x3100dC8464A8523306c3C5034de24a8927d6E590',
-    #     'rpc_url': 'https://rpc.blast.io'
-    # },
 ]
 
 
